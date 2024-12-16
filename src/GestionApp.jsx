9261d457d971d67cb299936c2687e4521aa813d5
william <wibepu12@gmail.com>
2024-12-16 12:02:50 -0500

    corregir responsive

--- src/GestionApp.jsx
+++ src/GestionApp.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import SistemaApp from './components/SistemaApp';
-import VisualProduct from './components/VisualProduct';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import SistemaApp from "./components/SistemaApp";
+import VisualProduct from "./components/VisualProduct";
 
 const GestionApp = () => {
   return (
-    <div>
- 
-      <nav>
-        <ul>
-          <li>
-            <Link to="/sistema">Sistema</Link>
-          </li>
-          <li>
-            <Link to="/visual">Productos</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="container">
+      <div className="row">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sistema">Sistema</Link>
+            </li>
+            <li>
+              <Link to="/visual">Productos</Link>
+            </li>
+          </ul>
+        </nav>
 
-
-      <Routes>
-        <Route path="/sistema" element={<SistemaApp />} />
-        <Route path="/visual" element={<VisualProduct />} />
-      </Routes>
+        <Routes>
+          <Route path="/sistema" element={<SistemaApp />} />
+          <Route path="/visual" element={<VisualProduct />} />
+        </Routes>
+      </div>
     </div>
   );
 };
