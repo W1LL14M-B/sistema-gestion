231dfd82c10352d00e1236f65fd4b008b7a36194
william <wibepu12@gmail.com>
2024-12-16 11:10:59 -0500

    optimizacion codigo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import SistemApp from './components/SistemaApp';
-//import AppWithProvider from './contex/AppWithProvider';
 import { ProductProvider } from './hooks/ProductContex';
 import GestionApp from './GestionApp';
 import { BrowserRouter } from 'react-router';
